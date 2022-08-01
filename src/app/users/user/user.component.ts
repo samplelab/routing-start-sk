7b6cf665790d42c169f630fbd203c0f455ca1a3a
satish kumar <satish.kumar.ee@gmail.com>
2022-08-01 16:13:57 +0530

    retrieving query parameters

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
+  paramSubscription : Subscription
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user={
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    }
+    this.paramSubscription = this.route.params.subscribe(
+      (params: Params)=>{
+        this.user.id=params['id'];
+        this.user.name=params['name'];
+      }
+
+    );
   }
 
 }
